2f86dfff94ac881622b9fd2bc8ee8a3069690beb
JungCheolHoon <cjswovkdnj12@naver.com>
2022-08-02 17:51:09 +0900

    Update

---
+++ src/VariableArgumentDemo.java
@@ -0,0 +1,9 @@
+
+public class VariableArgumentDemo {
+	public static void main(String[] args) {
+		mymethod(1,2,3,4,5,6,7);	//Argument - 넘겨주는 변수
+	}
+	static void mymethod(int ... array) {	//parameter 받는 변수
+		
+	}
+}
