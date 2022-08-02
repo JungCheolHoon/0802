028237be3e556ec4e48ea9caea3170a6705cb490
JungCheolHoon <cjswovkdnj12@naver.com>
2022-08-02 18:53:32 +0900

    Update

--- src/ArrayDemo1.java
+++ src/ArrayDemo1.java
@@ -1,6 +1,27 @@
 
 public class ArrayDemo1 {
 	public static void main(String[] args) {
+		
+		int [][] array1 = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
+		int [][] array2 = {{1,2,3},{4,5,6},{7,8,9}};
+		
+		for(int i = 0 ; i < array1.length ; i++) {
+			for(int j = 0 ; j < array1[i].length ; j++) {
+				System.out.print(array1[i][j]);
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		for(int i = 0 ; i < array2.length ; i++) {
+			for(int j = 0 ; j < array2[i].length ; j++) {
+				System.out.print(array2[i][j]);
+			}
+			
+			System.out.println();
+		}
+		
 //		<<Rectangular Array>>
 //		int [][] array = null;		//Declaration
 //		array = new int[2][3];	//Creation
@@ -47,18 +68,18 @@ public class ArrayDemo1 {
 //		array[2] = new int[] {11,12,13,14};
 			
 //		3st way
-		int[][] array = {
-				{3,4,5,6},
-				{7,8},
-				{9,10,11,12,13,14}
-		};
-		
-		for(int i = 0 ; i<array.length; i++) {
-			for(int j = 0 ; j <array[i].length ; j ++) {
-				System.out.print(array[i][j]+"\t");
-			}
-			System.out.println();
-		}
+//		int[][] array = {
+//				{3,4,5,6},
+//				{7,8},
+//				{9,10,11,12,13,14}
+//		};
+//		
+//		for(int i = 0 ; i<array.length; i++) {
+//			for(int j = 0 ; j <array[i].length ; j ++) {
+//				System.out.print(array[i][j]+"\t");
+//			}
+//			System.out.println();
+//		}
 		
 	}
 }
